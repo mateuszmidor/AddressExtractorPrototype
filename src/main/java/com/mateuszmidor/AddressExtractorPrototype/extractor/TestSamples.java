4200fce7e8f533fc7584eea42e9b2a37805135fd
mateusz <kontakt@mateuszmidor.com>
2015-02-01 12:01:50 +0100

    TestSamples can now saveToFile

--- src/main/java/com/mateuszmidor/AddressExtractorPrototype/extractor/TestSamples.java
+++ src/main/java/com/mateuszmidor/AddressExtractorPrototype/extractor/TestSamples.java
@@ -1,6 +1,8 @@
 package com.mateuszmidor.AddressExtractorPrototype.extractor;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.HashSet;
@@ -11,11 +13,10 @@ import java.util.Set;
 /**
  * 
  * @author mateusz This class is basically a collection of test samples. It
- *         allows to read test samples from file.
+ *         allows to read test samples from file/save samples to file.
  */
 public class TestSamples extends LinkedList<TestSample> {
     private static final long serialVersionUID = 2013396258330884057L;
-
 
     public static TestSamples fromFile(final String filename) throws IOException {
         TestSamples result = new TestSamples();
@@ -64,11 +65,34 @@ public class TestSamples extends LinkedList<TestSample> {
         return "";
     }
     
+    public void saveToFile(String filename) {
+        try (PrintWriter pw = new PrintWriter(new FileWriter(filename))) {
+            printSamples(pw);
+        } catch (IOException e) {
+            System.out.println("Error while writing file " + filename);
+        }
+    }
+    
+    private void printSamples(PrintWriter pw) {
+        int i = 1;
+        for (TestSample s : this) {
+            pw.println("# " + i + ".");
+            for (String src : s.sources) {
+                pw.println("source=" + src);
+            }
+            pw.println("expected=" + s.expected_result);
+            i++;
+        }
+        
+    }
+
+    /**
+     * Removes test samples that have same contents (fiels: sources)
+     */
     public void removeDuplicates() {
         Collections.sort(this);
         Set<TestSample> noDuplicates = new HashSet<TestSample>(this);
         this.clear();
         this.addAll(noDuplicates);
-        
     }
 }
