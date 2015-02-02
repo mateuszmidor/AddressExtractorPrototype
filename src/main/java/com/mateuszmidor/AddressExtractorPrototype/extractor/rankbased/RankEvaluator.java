646ac7da08b53d5736d628013411d1098ca37719
Mateusz Midor <m.midor@samsung.com>
2015-02-02 16:06:56 +0100

    Added RankEvaluator

---
+++ src/main/java/com/mateuszmidor/AddressExtractorPrototype/extractor/rankbased/RankEvaluator.java
@@ -0,0 +1,12 @@
+package com.mateuszmidor.AddressExtractorPrototype.extractor.rankbased;
+
+/**
+ * Interface for evaluators that rank ExtractionResult's precisionRank and correctnessRank
+ * based on criteria like capital letter (eg. "Wielicka" vs "wielicka", 
+ * context (eg. "ulica Wielicka"), presence of number (eg. "Wielicka 23")
+ * @author m.midor
+ *
+ */
+public interface RankEvaluator {
+	void evaluate(ExtractionResult r);
+}
